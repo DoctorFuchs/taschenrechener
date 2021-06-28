6b84d3825f8ef00a79bd9a70ca0083433aed145e
Matthias Fuchs <61468448+DoctorFuchs@users.noreply.github.com>
2021-06-28 19:28:35 +0000

    28.06.2021

---
+++ database.py
@@ -0,0 +1,424 @@
+Elements = {
+	"H":{
+		"Name":"Wasserstoff",
+		"Ordnungszahl": 1,
+		"Atommasse":1.008
+	},
+	"He":{
+		"Name":"Helium",
+		"Ordnungszahl": 2,
+		"Atommasse":4.0026
+	},
+	"Li":{
+		"Name":"Lithium",
+		"Ordnungszahl": 3,
+		"Atommasse":6.997
+	},
+	"Be":{
+		"Name":"Beryllium",
+		"Ordnungszahl": 4,
+		"Atommasse":9.0122
+	},
+	"B":{
+		"Name":"Bor",
+		"Ordnungszahl": 5,
+		"Atommasse":10.806
+	},
+	"C":{
+		"Name":"Kohlenstoff",
+		"Ordnungszahl": 6,
+		"Atommasse":12.009
+	},
+	"N":{
+		"Name":"Sickstoff",
+		"Ordnungszahl": 7,
+		"Atommasse":14.007
+	},
+	"O":{
+		"Name":"Sauerstoff",
+		"Ordnungszahl": 8,
+		"Atommasse":15.999
+	},
+	"F":{
+		"Name":"Flour",
+		"Ordnungszahl": 9,
+		"Atommasse":18.998
+	},
+	"Ne":{
+		"Name":"Neon",
+		"Ordnungszahl": 10,
+		"Atommasse":20.180
+	},
+	"Na":{
+		"Name":"Natrium",
+		"Ordnungszahl": 11,
+		"Atommasse":22.990
+	},
+	"Mg":{
+		"Name":"Magnesium",
+		"Ordnungszahl": 12,
+		"Atommasse":24.304
+	},
+	"Al":{
+		"Name":"Aluminium",
+		"Ordnungszahl": 13,
+		"Atommasse":26.982
+	},
+
+	"Si":{
+		"Name":"Silicium",
+		"Ordnungszahl": 14,
+		"Atommasse":28.084
+	},
+	"P":{
+		"Name":"Phosphor",
+		"Ordnungszahl": 15,
+		"Atommasse":30.974
+	},
+	"S":{
+		"Name":"Schwefel",
+		"Ordnungszahl": 16,
+		"Atommasse":32.059
+	},
+	"Cl":{
+		"Name":"Chlor",
+		"Ordnungszahl": 17,
+		"Atommasse":35.446
+	},
+	"Ar":{
+		"Name":"Argon",
+		"Ordnungszahl": 18,
+		"Atommasse":39.948
+	},
+	"K":{
+		"Name":"Kalium",
+		"Ordnungszahl":19,
+		"Atommasse":39.098
+	},
+	"Ca":{
+		"Name":"Cacium",
+		"Ordnungszahl":20,
+		"Atommasse":40.078
+	},
+	"Sc":{
+		"Name":"Scandium",
+		"Ordnungszahl":21,
+		"Atommasse":44.956
+	},
+	"Ti":{
+		"Name":"Titan",
+		"Ordnungszahl":22,
+		"Atommasse":47.867
+	},
+	"V":{
+		"Name":"Vanadium",
+		"Ordnungszahl":23,
+		"Atommasse":50.94
+	},
+	"Cr":{
+		"Name":"Chrom",
+		"Ordnungszahl": 24,
+		"Atommasse": 51.996
+	},
+	"Mn":{
+		"Name":"Mangan",
+		"Ordnungszahl": 25,
+		"Atommasse":54.94
+	},
+	"Fe":{
+		"Name":"Eisen",
+		"Ordnungszahl": 26,
+		"Atommasse":55.85
+	},
+	"Co":{
+		"Name":"Cobalt",
+		"Ordnungszahl": 27,
+		"Atommasse":58.93
+	},
+	"Ni":{
+		"Name":"Nickel",
+		"Ordnungszahl": 28,
+		"Atommasse":58.69
+	},
+	"Cu":{
+		"Name":"Kupfer",
+		"Ordnungszahl": 29,
+		"Atommasse":63.55
+	},
+	"Zn":{
+		"Name":"Zink",
+		"Ordnungszahl": 30,
+		"Atommasse":65.39
+	},
+	"Ga":{
+		"Name":"Gallium",
+		"Ordnungszahl": 31,
+		"Atommasse":69.72
+	},
+	"Ge":{
+		"Name":"Germanium",
+		"Ordnungszahl": 32,
+		"Atommasse":72.61
+	},
+	"As":{
+		"Name":"Arsen",
+		"Ordnungszahl": 33,
+		"Atommasse":74.92
+	},
+	"Se":{
+		"Name":"Selen",
+		"Ordnungszahl": 34, 
+		"Atommasse":78.96
+	},
+	"Br":{
+		"Name":"Brom",
+		"Ordnungszahl": 35, 
+		"Atommasse": 79.90
+	},
+	"Kr":{
+		"Name":"Krypton",
+		"Ordnungszahl": 36,
+		"Atommasse":83.89
+	},
+	"Rb":{
+		"Name":"Rubidium",
+		"Ordnungszahl": 37,
+		"Atommasse": 85.47
+	},
+	"Sr":{
+		"Name":"Strontium",
+		"Ordnungszahl": 38,
+		"Atommasse":87.62
+	},
+	"Y":{
+		"Name":"Yttrium",
+		"Ordnungszahl": 39,
+		"Atommasse":88.91
+	},
+	"Zr":{
+		"Name":"Zicronium",
+		"Ordnungszahl": 40,
+		"Atommasse": 91.22
+	},
+	"Nb":{
+		"Name":"Niob",
+		"Ordnungszahl": 41,
+		"Atommasse": 92.91
+	},
+	"Mo":{
+		"Name":"Molybdän",
+		"Ordnungszahl": 42,
+		"Atommasse":95.94
+	},
+	"Tc":{
+		"Name":"Technetium",
+		"Ordnungszahl": 43,
+		"Atommasse":98.00
+	},
+	"Ru":{
+		"Name":"Ruthenium",
+		"Ordnungszahl": 44,
+		"Atommasse": 101.07
+	},
+	"Rh":{
+		"Name":"Rhodium", 
+		"Ordnungszahl": 45,
+		"Atommasse":102.91
+	},
+	"Pd":{
+		"Name":"Palladium",
+		"Ordnungszahl": 46,
+		"Atommasse":106.42
+	},
+	"Ag":{
+		"Name":"Silber",
+		"Ordnungszahl": 47,
+		"Atommasse":107.87
+	},
+	"Cd":{
+		"Name":"Cadmium",
+		"Ordnungszahl": 48,
+		"Atommasse": 112.41
+	},
+	"In":{
+		"Name":"Indium",
+		"Ordnungszahl": 49,
+		"Atommasse": 113.82
+	},
+	"Sn":{
+		"Name":"Zinn",
+		"Ordnungszahl": 50,
+		"Atommasse": 118.71
+	},
+	"Sb":{
+		"Name":"Antimon",
+		"Ordnungszahl": 51,
+		"Atommasse": 121.76
+	},
+	"Te":{
+		"Name":"Tellur",
+		"Ordnungszahl": 52,
+		"Atommasse": 127.6
+	},
+	"I":{
+		"Name":"Iod",
+		"Ordnungszahl": 53,
+		"Atommasse": 126.9
+	},
+	"Xe":{
+		"Name":"Xenon",
+		"Ordnungszahl": 54,
+		"Atommasse": 131.29
+	},
+	"Cs":{
+		"Name":"Caesium",
+		"Ordnungszahl": 55,
+		"Atommasse": 132.91
+	},
+	"Ba":{
+		"Name":"Barium",
+		"Ordnungszahl": 56,
+		"Atommasse": 137.33
+	},
+	"La":{
+		"Name":"Lanthan",
+		"Ordnungszahl": 57,
+		"Atommasse": 138.91
+	},
+	"Hf":{
+		"Name":"Hafnium",
+		"Ordnungszahl": 72,
+		"Atommasse": 178.49
+	},
+	"Ta":{
+		"Name":"Tantal",
+		"Ordnungszahl": 73,
+		"Atommasse": 180.95
+	},
+	"W":{
+		"Name":"Wolfram",
+		"Ordnungszahl": 74,
+		"Atommasse": 183.84
+	},
+	"Re":{
+		"Name":"Rhenium",
+		"Ordnungszahl": 75,
+		"Atommasse": 186.21
+	},
+	"Os":{
+		"Name":"Osmium",
+		"Ordnungszahl": 76,
+		"Atommasse": 190.23
+	},
+	"Ir":{
+		"Name":"Iridium",
+		"Ordnungszahl":77,
+		"Atommasse": 192.22
+	},
+	"Pt":{
+		"Name":"Platin",
+		"Ordnungszahl":78,
+		"Atommasse": 195.08
+	},
+	"Au":{
+		"Name":"Gold",
+		"Ordnungszahl":79,
+		"Atommasse": 196.97
+	},
+	"Hg":{
+		"Name":"Quecksilber",
+		"Ordnungszahl":80,
+		"Atommasse": 200.59
+	},
+	"Tl":{
+		"Name":"Thallium",
+		"Ordnungszahl": 81,
+		"Atommasse": 204.38
+	},
+	"Pb":{
+		"Name":"Blei",
+		"Ordnungszahl": 81,
+		"Atommasse": 204.38
+	},
+	"Bi":{
+		"Name":"Bismut",
+		"Ordnungszahl": 83,
+		"Atommasse":208.98
+	},
+	"Po":{
+		"Name":"Polonium",
+		"Ordnungszahl": 84,
+		"Atommasse": 209
+	},
+	"At":{
+		"Name":"Astat",
+		"Ordnungszahl": 85,
+		"Atommasse": 210
+	},
+	"Rn":{
+		"Name":"Radon",
+		"Ordnungszahl": 86,
+		"Atommasse": 222
+	},
+	"Fr":{
+		"Name":"Francium",
+		"Ordnungszahl": 87,
+		"Atommasse": 223
+	},
+	"Ra":{
+		"Name":"Radium",
+		"Ordnungszahl": 88,
+		"Atommasse": 226.03
+	},
+	"Rf":{
+		"Name":"Rutherfordium",
+		"Ordnungszahl": 104,
+		"Atommasse": 261
+	},
+	"Db":{
+		"Name":"Dubnium",
+		"Ordnungszahl": 105,
+		"Atommasse": 262
+	},
+	"Sg":{
+		"Name":"Seaborgium",
+		"Ordnungszahl": 106,
+		"Atommasse": 266
+	},
+	"Bh":{
+		"Name":"Bohrium",
+		"Ordnungszahl": 107,
+		"Atommasse": 264
+	},
+	"Hs":{
+		"Name":"Hassium",
+		"Ordnungszahl": 108,
+		"Atommasse": 269
+	},
+	"Mt":{
+		"Name":"Meitnerium",
+		"Ordnungszahl": 109,
+		"Atommasse": 268
+	},
+	"Ds":{
+		"Name":"Darmstadium",
+		"Ordnungszahl": 110,
+		"Atommasse": 271
+	},
+	"Rg":{
+		"Name":"Roentgenium",
+		"Ordnungszahl": 111,
+		"Atommasse": 272
+	},
+	"Cn":{
+		"Name":"Copernicum",
+		"Ordnungszahl": 112,
+		"Atommasse": 277
+	},
+	"Uuq":{
+		"Name":"Ununquadium",
+		"Ordnungszahl": 114,
+		"Atommasse":289
+	}
+}
+
---
+++ main.py
@@ -0,0 +1,118 @@
+import database as data
+
+
+while True:
+    print("""\nChemie Rechner Tool
+    1. Übersicht
+    2. Rechner
+    3. Credits""")
+    mode = input("Modus: ")
+    if mode == "1":
+        print("""Konstanten:
+Avogadro-Konstante = \n6e23
+""")
+        input("Weiter (Formeln)? ")
+        print("""Formeln:
+Masse = \nMolare Masse * Stoffmenge; \nm = M * n
+""")
+        input("Weiter (Formeln 2)? ")
+        print("""Absolute Teilchenanzahl = \nStoffmenge * Molare Masse; \nN = n * M """)
+        input("Weiter (Variablen 1)? ")
+        print("""Formelzeichen:
+M = Molare Masse\ng/Mol oder u
+m = Masse in g
+ma = Atommasse in u 
+""")
+        input("Weiter (Variablen 2)?")
+        print("""Mol = 6*10**23 Teilchen 
+N = Absolute Teilchen-\nanzahl; Teilchena
+n = Stoffmenge einer\nStoffportion in Mol
+Na = Avogadro-Konstante""")
+        input("Ende? ")
+    
+    elif mode == "2":
+        def MolareMasseR():
+            Masse1 = input('Masse in g:')
+            Masse1 = float(Masse1)
+            Stoffmenge1 = input('Stoffmenge in Mol: ')
+            Stoffmenge1 = int(Stoffmenge1)
+            MolareMasse1 = Masse1 / Stoffmenge1
+            print(str(MolareMasse1) + 'g/Mol')
+
+
+        def MasseR():
+            while True:
+                stoff = input("Hast du einen Stoff? \n(1-Ja / 2-Nein): ")
+                if stoff == "1":
+                    hatStoff = True
+                    break
+
+                elif stoff == "2":
+                    hatStoff = False
+                    break
+
+            if not hatStoff:
+                MolareMasse2 = float(input('Molare Masse in g/Mol: '))
+            else:
+                MolareMasse2 = input("Element \n(Formelzeichen): ")
+                MolareMasse2 = data.Elements[MolareMasse3]["Atommasse"]
+            Stoffmenge2 = input('Stoffmenge in Mol: ')
+            Stoffmenge2 = float(Stoffmenge2)
+            Masse2 = MolareMasse2 * Stoffmenge2
+            print(str(Masse2) + 'kg')
+
+
+        def StoffmengeR():
+            while True:
+                stoff = input("Hast du einen Stoff?\n (1-Ja / 2-Nein): ")
+                if stoff == "1":
+                    hatStoff = True
+                    break
+
+                elif stoff == "2":
+                    hatStoff = False
+                    break
+
+            if not hatStoff:
+                MolareMasse3 = float(input('Molare Masse \nin g/Mol: '))
+            else:
+                MolareMasse3 = input("Element (Formelzeichen): ")
+                MolareMasse3 = data.Elements[MolareMasse3]["Atommasse"]
+            Masse3 = float(input('Masse in g:'))
+            Stoffmenge3 = Masse3 / MolareMasse3
+            print(str(Stoffmenge3) + 'Mol')
+            input("Weiter? ")
+            F2 = input('Willst du die \nStoffmenge in die \nabsolute Teilchenanzahl (N) \numrechnen? (1-Ja/2-Nein)')
+            if F2 == '1':
+                Stoffmenge3 = float(Stoffmenge3)
+                AbsoluteTeilchenanzahl = Stoffmenge3 * 6 * (10 ** 23)
+                print(AbsoluteTeilchenanzahl)
+            else:
+                print('OK')
+
+        while True:
+            F = input('''Was willst du ausrechnen? 
+\tMolare Masse(1) 
+\tMasse(2)
+\tStoffmenge(3)\nModus: ''')
+
+            try:
+                if 1 <= int(F) <= 3:
+                    break
+
+            except:
+                print("nicht verfügbar")
+
+        if F == '1':
+            MolareMasseR()
+        elif F == '2':
+            MasseR()
+        else:
+            StoffmengeR()
+
+    elif mode == "3":
+        print("""Developed by\nJohannes R.\nBenedek T.\nDoctorFuchs""")
+        input("Ende? ")
+    
+    else:
+        print("nicht verfügbar!")
